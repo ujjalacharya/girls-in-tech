c04062c5f1878a8bd15f8eea3ecec962d842ace9
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-04-16 22:18:05 +0545

    initial commit

---
+++ controllers/indexController.js
@@ -0,0 +1,21 @@
+exports.getIndex = (req, res) => {
+ res.render('index');
+}
+exports.getDashboard = (req, res) => {
+ res.render('dashboard', {title: 'dashboard'});
+}
+exports.getHelpline = (req, res) => {
+ res.render('helpline', {title: 'helpline'});
+}
+exports.getEvents = (req, res) => {
+ res.render('events', {title: 'events'});
+}
+exports.getBlogs = (req, res) => {
+ res.render('blogs', {title: 'blogs'});
+}
+exports.getProfile = (req, res) => {
+ res.render('profile', {title: 'profile'});
+}
+exports.getAbout = (req, res) => {
+ res.render('about', {title: 'about'});
+}
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+let indexController = require('../controllers/indexController');
+
+/* GET home page. */
+router.get('/', indexController.getIndex);
+
+router.get('/dashboard',indexController.getDashboard);
+router.get('/helpline', indexController.getHelpline);
+router.get('/events', indexController.getEvents);
+router.get('/blogs', indexController.getBlogs);
+router.get('/profile', indexController.getProfile);
+router.get('/about', indexController.getAbout);
+
+module.exports = router;
