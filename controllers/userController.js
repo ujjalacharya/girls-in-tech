b60b528a7c0cab55bed39ab073082dc2867d785e
AanandBhandari <aanandbhandari143@gmail.com>
2019-04-19 19:13:02 +0545

    bootstrap issue solve and add firebase

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,26 @@
+var firebase = require("firebase/app");
+var database = firebase.database();
 exports.getSignup = (req, res) => {
     res.render('signup');
 }
 exports.postSignup = (req, res) => {
+    function writeUserData(name, email) {
+        firebase.database().ref('/users').set({
+            username: name,
+            email: email
+        });
+    }
+    writeUserData(req.body.name,req.body.email)
     res.redirect('/users/profile');
 }
 exports.profile = (req, res) => {
-    res.render('profile', { title: 'profile' });
+    let users = firebase.database().ref('/users/');
+    users.on('value',function (user) {
+        console.log(user.val());
+    })
+    // .then(user => {
+    //     console.log(user);
+    //     users.off('value')
+    // }).catch(e=>console.log(e))
+    res.render('profile');
 }
