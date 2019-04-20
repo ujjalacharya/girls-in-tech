cbb51314ced9a8517103cbc22876780965eb76fb
Asim Dahal <dahalasim1997@gmail.com>
2019-04-20 12:57:30 +0545

    dashboard added with search functionality

---
+++ controllers/mock/mockdata.js
@@ -0,0 +1,33 @@
+const mockData = [{
+  imageUrl: 'https://www.artsoft-consult.ro/wp-content/uploads/2018/05/artsoft-fb-android-dev-job_659x263.png', heading: 'Android development workshop', content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit sunt perferendis magnam reiciendis iusto commodi
+        praesentium minus ea quae eligendi eius, omnis id dignissimos laboriosam quidem voluptatibus nihil velit
+        accusamus.` }, {
+  imageUrl: 'https://www.wecode.tn/wp-content/uploads/2018/09/ios-dev.jpg', heading: 'Ios development workshop', content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit sunt perferendis magnam reiciendis iusto commodi
+        praesentium minus ea quae eligendi eius, omnis id dignissimos laboriosam quidem voluptatibus nihil velit
+        accusamus.`}, {
+  imageUrl: 'https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png', heading: 'React js development workshop', content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit sunt perferendis magnam reiciendis iusto commodi
+        praesentium minus ea quae eligendi eius, omnis id dignissimos laboriosam quidem voluptatibus nihil velit
+        accusamus.`},
+{
+  imageUrl: 'https://mightydeals.s3.amazonaws.com/alt-teaser-images/2994/Become-the-Ultimate-Python-Developer.png', heading: 'Python development bootcamp', content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit sunt perferendis magnam reiciendis iusto commodi
+        praesentium minus ea quae eligendi eius, omnis id dignissimos laboriosam quidem voluptatibus nihil velit
+        accusamus.` }, {
+  imageUrl: 'https://cdn-images-1.medium.com/max/1000/1*GkR93AAlILkmE_3QQf88Ug.png', heading: 'React native development', content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit sunt perferendis magnam reiciendis iusto commodi
+        praesentium minus ea quae eligendi eius, omnis id dignissimos laboriosam quidem voluptatibus nihil velit
+        accusamus.`}, {
+  imageUrl: 'https://www.sitepen.com/blog/wp-content/uploads/2017/09/blog.jpg', heading: 'Angular bootcamp', content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit sunt perferendis magnam reiciendis iusto commodi
+        praesentium minus ea quae eligendi eius, omnis id dignissimos laboriosam quidem voluptatibus nihil velit
+        accusamus.`},
+{
+  imageUrl: 'https://i.ytimg.com/vi/DsuTwV0jwaY/maxresdefault.jpg', heading: 'VUE bootcamp', content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit sunt perferendis magnam reiciendis iusto commodi
+        praesentium minus ea quae eligendi eius, omnis id dignissimos laboriosam quidem voluptatibus nihil velit
+        accusamus.` }, {
+  imageUrl: 'https://i.ytimg.com/vi/2yXfUPwlZTw/maxresdefault.jpg', heading: 'Python django course', content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit sunt perferendis magnam reiciendis iusto commodi
+        praesentium minus ea quae eligendi eius, omnis id dignissimos laboriosam quidem voluptatibus nihil velit
+        accusamus.`}, {
+  imageUrl: 'https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png', heading: 'React js development workshop', content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit sunt perferendis magnam reiciendis iusto commodi
+        praesentium minus ea quae eligendi eius, omnis id dignissimos laboriosam quidem voluptatibus nihil velit
+        accusamus.`},
+];
+
+module.exports = mockData;
--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,14 @@ let indexController = require('../controllers/indexController');
 /* GET home page. */
 router.get('/', indexController.getIndex);
 
-router.get('/dashboard',indexController.getDashboard);
+router.get('/dashboard', indexController.getDashboard);
 router.get('/helpline', indexController.getHelpline);
 router.get('/events', indexController.getEvents);
 router.get('/blogs', indexController.getBlogs);
-router.post('/helpline',indexController.postHelpline);
+router.post('/helpline', indexController.postHelpline);
 router.get('/about', indexController.getAbout);
 router.get('/roadmap', indexController.getRoadmap);
+
+router.post('/handleSearch', indexController.handleSearch);
 
 module.exports = router;
